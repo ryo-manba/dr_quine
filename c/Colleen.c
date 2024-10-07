c7f20236efa380d6349322134e576d01d8fe8eb1
Ryo Matsukawa <76232929+ryo-manba@users.noreply.github.com>
2024-10-07 09:14:51 +0900

    add Colleen.c

---
+++ c/Colleen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+  10: newline, 34: semicolon
+*/
+void quine() {
+  const char *s = "#include <stdio.h>%c%c/*%c  10: newline, 34: semicolon%c*/%cvoid quine() {%c  const char *s = %c%s%c;%c  printf(s,10,10,10,10,10,10,34,s,34,10,10,10,10,10,10,10,10,10,10,10);%c}%c%cint main(void) {%c  /*%c    comment in main%c  */%c  quine();%c  return 0;%c}%c";
+  printf(s,10,10,10,10,10,10,34,s,34,10,10,10,10,10,10,10,10,10,10,10);
+}
+
+int main(void) {
+  /*
+    comment in main
+  */
+  quine();
+  return 0;
+}
