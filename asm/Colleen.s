365102c912dced7bd52be8af55e4f74b15b1a64e
Ryo Matsukawa <76232929+ryo-manba@users.noreply.github.com>
2024-10-08 11:57:13 +0900

    fix: add null terminator

--- asm/Colleen.s
+++ asm/Colleen.s
@@ -21,4 +21,4 @@ print:
   ret
 
 section .data
-  self: db "section .text%1$c  default rel%1$c  extern printf%1$c  global main%1$c%1$cmain:%1$c  push rbp%1$c  call print%1$c  pop rbp%1$c  mov rax, 0%1$c  ret%1$c%1$c; sub function%1$cprint:%1$c  mov	rdi, self%1$c  mov	rsi, 10 ; newline%1$c  mov rdx, 34 ; double quote%1$c  mov rcx, self%1$c  mov	rax, 0%1$c  call printf wrt ..plt%1$c  ret%1$c%1$csection .data%1$c  self: db %2$c%3$s%2$c%1$c"
+  self: db "section .text%1$c  default rel%1$c  extern printf%1$c  global main%1$c%1$cmain:%1$c  push rbp%1$c  call print%1$c  pop rbp%1$c  mov rax, 0%1$c  ret%1$c%1$c; sub function%1$cprint:%1$c  mov	rdi, self%1$c  mov	rsi, 10 ; newline%1$c  mov rdx, 34 ; double quote%1$c  mov rcx, self%1$c  mov	rax, 0%1$c  call printf wrt ..plt%1$c  ret%1$c%1$csection .data%1$c  self: db %2$c%3$s%2$c, 0%1$c", 0
