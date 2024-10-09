2ba815347d5a546b6fb21c1c9af24d6ad96968c4
Ryo Matsukawa <76232929+ryo-manba@users.noreply.github.com>
2024-10-09 15:15:21 +0900

    replace Grace.s with macros

--- asm/Grace.s
+++ asm/Grace.s
@@ -1,5 +1,8 @@
-extern dprintf
+%define FILENAME "Grace_kid.s"
+%define SELF "%%define FILENAME %2$cGrace_kid.s%2$c%1$c%%define SELF %2$c%3$s%2$c%1$c%1$c%%macro MAIN 0%1$cextern dprintf%1$csection .text%1$c  global main%1$c%1$cmain:%1$c  lea rdi, [rel filename]%1$c  mov rsi, 0101o  ; O_CREAT | O_WRONLY%1$c  mov rax, 2      ; sys_open%1$c  syscall%1$c%1$c  cmp rax, 0%1$c  jl error%1$c%1$c  push rax  ; save file descriptor (for close)%1$c%1$c  mov rdi, rax%1$c  lea rsi, [rel format]%1$c  mov rdx, 10%1$c  mov rcx, 34%1$c  lea r8, [rel format]%1$c  call dprintf wrt ..plt%1$c%1$c  pop rdi    ; restore file descriptor%1$c  mov rax, 3 ; sys_close%1$c  syscall%1$c%1$c  xor rax, rax%1$c  ret%1$c%1$cerror:%1$c  mov rax, 1%1$c  ret%1$c%1$csection .data%1$c  format: db SELF, 0%1$c  filename: db FILENAME, 0%1$c%%endmacro%1$c%1$cMAIN%1$c"
 
+%macro MAIN 0
+extern dprintf
 section .text
   global main
 
@@ -33,5 +36,8 @@ error:
   ret
 
 section .data
-  format: db "extern dprintf%1$c%1$csection .text%1$c  global main%1$c%1$cmain:%1$c  lea rdi, [rel filename]%1$c  mov rsi, 0101o  ; O_CREAT | O_WRONLY%1$c  mov rax, 2      ; sys_open%1$c  syscall%1$c%1$c  cmp rax, 0%1$c  jl error%1$c%1$c  push rax  ; save file descriptor (for close)%1$c%1$c  mov rdi, rax%1$c  lea rsi, [rel format]%1$c  mov rdx, 10%1$c  mov rcx, 34%1$c  lea r8, [rel format]%1$c  call dprintf wrt ..plt%1$c%1$c  pop rdi    ; restore file descriptor%1$c  mov rax, 3 ; sys_close%1$c  syscall%1$c%1$c  xor rax, rax%1$c  ret%1$c%1$cerror:%1$c  mov rax, 1%1$c  ret%1$c%1$csection .data%1$c  format: db %2$c%3$s%2$c, 0%1$c  filename: db %2$cGrace_kid.s%2$c, 0%1$c", 0
-  filename: db "Grace_kid.s", 0
+  format: db SELF, 0
+  filename: db FILENAME, 0
+%endmacro
+
+MAIN
