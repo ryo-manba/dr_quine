a05c9b9556ca79fba5554480f521ea1e938d9c3d
Ryo Matsukawa <76232929+ryo-manba@users.noreply.github.com>
2024-10-10 11:40:57 +0900

    add Sully.s

--- asm/setup.sh
+++ asm/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build --platform linux/amd64 -t nasm-env .
-docker run --rm -it --platform linux/amd64 -v $(pwd):/usr/workspace nasm-env:latest
+docker run --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --rm -it --platform linux/amd64 -v $(pwd):/usr/workspace nasm-env:latest
