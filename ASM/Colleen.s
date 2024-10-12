eba650c2861a41ac3164bc93bfe55f7d98eb7978
Ryo Matsukawa <76232929+ryo-manba@users.noreply.github.com>
2024-10-12 12:56:12 +0900

    add function prologue and epilogue

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -4,22 +4,27 @@ section .text
 
 main:
   push rbp
+  mov rbp, rsp
   call print
+  xor rax, rax
+  mov rsp, rbp
   pop rbp
-  mov rax, 0
   ret
 
 ; sub function
 print:
+  push rbp
+  mov rbp, rsp
   lea rdi, [rel self]
   mov	rsi, 10 ; newline
   mov rdx, 34 ; double quote
-  lea rcx, [rel self]
+  mov rcx, rdi
   mov	rax, 0
   call printf wrt ..plt
+  leave
   ret
 
 section .data
-  self: db "section .text%1$c  extern printf%1$c  global main%1$c%1$cmain:%1$c  push rbp%1$c  call print%1$c  pop rbp%1$c  mov rax, 0%1$c  ret%1$c%1$c; sub function%1$cprint:%1$c  lea rdi, [rel self]%1$c  mov	rsi, 10 ; newline%1$c  mov rdx, 34 ; double quote%1$c  lea rcx, [rel self]%1$c  mov	rax, 0%1$c  call printf wrt ..plt%1$c  ret%1$c%1$csection .data%1$c  self: db %2$c%3$s%2$c, 0%1$c%1$csection .note.GNU-stack%1$c", 0
+  self: db "section .text%1$c  extern printf%1$c  global main%1$c%1$cmain:%1$c  push rbp%1$c  mov rbp, rsp%1$c  call print%1$c  xor rax, rax%1$c  mov rsp, rbp%1$c  pop rbp%1$c  ret%1$c%1$c; sub function%1$cprint:%1$c  push rbp%1$c  mov rbp, rsp%1$c  lea rdi, [rel self]%1$c  mov	rsi, 10 ; newline%1$c  mov rdx, 34 ; double quote%1$c  mov rcx, rdi%1$c  mov	rax, 0%1$c  call printf wrt ..plt%1$c  leave%1$c  ret%1$c%1$csection .data%1$c  self: db %2$c%3$s%2$c, 0%1$c%1$csection .note.GNU-stack%1$c", 0
 
 section .note.GNU-stack
--- ASM/Sully.s
+++ ASM/Sully.s
@@ -6,6 +6,10 @@ section .text
   global main
 
 main:
+  push rbp
+  mov rbp, rsp
+  sub rsp, 32
+
   mov rcx, 5
 
 %ifdef CHILD
@@ -73,17 +77,19 @@ main:
 
 error:
   mov rax, 1
+  leave
   ret
 
 done:
   xor rax, rax
+  leave
   ret
 
 section .data
   filename_format: db "Sully_%d.s", 0
   compile_cmd_format: db "nasm -f elf64 -d CHILD Sully_%1$d.s -o Sully_%1$d.o && gcc -Wall -Wextra -Werror Sully_%1$d.o -o Sully_%1$d", 0
   exec_cmd_format: db "./Sully_%d", 0
-  format: db "extern dprintf%1$cextern snprintf%1$cextern system%1$c%1$csection .text%1$c  global main%1$c%1$cmain:%1$c  mov rcx, %4$d%1$c%1$c%%ifdef CHILD%1$c  dec rcx%1$c%%endif%1$c%1$c  cmp rcx, 0%1$c  jl done%1$c%1$c  push rcx ; save counter%1$c  push rcx ; save counter%1$c  push rcx ; save counter%1$c%1$c  ; create file name%1$c  lea rdi, [rel filename]%1$c  mov rsi, 20%1$c  lea rdx, [rel filename_format]%1$c  call snprintf wrt ..plt%1$c%1$c  ; open file%1$c  lea rdi, [rel filename]%1$c  mov rsi, 1101o ; O_CREAT | O_WRONLY | O_TRUNC%1$c  mov rax, 2     ; sys_open%1$c  syscall%1$c%1$c  cmp rax, 0%1$c  jl error%1$c  pop r9 ; restore counter for file format%1$c  push rax ; store file descriptor%1$c%1$c  ; write file%1$c  mov rdi, rax%1$c  lea rsi, [rel format]%1$c  mov rdx, 10%1$c  mov rcx, 34%1$c  lea r8, [rel format]%1$c  call dprintf wrt ..plt%1$c%1$c  ; close file%1$c  pop rdi    ; restore file descriptor%1$c  mov rax, 3 ; sys_close%1$c  syscall%1$c%1$c  ; run compile%1$c  lea rdi, [rel compile_cmd]%1$c  mov rsi, 100%1$c  lea rdx, [rel compile_cmd_format]%1$c  pop rcx ; restore counter%1$c  call snprintf wrt ..plt%1$c%1$c  lea rdi, [rel compile_cmd]%1$c  call system wrt ..plt%1$c%1$c  ; execute file%1$c  lea rdi, [rel exec_cmd]%1$c  mov rsi, 100%1$c  lea rdx, [rel exec_cmd_format]%1$c  pop rcx ; restore counter%1$c  call snprintf wrt ..plt%1$c%1$c  lea rdi, [rel exec_cmd]%1$c  call system wrt ..plt%1$c%1$c  jmp done%1$c%1$cerror:%1$c  mov rax, 1%1$c  ret%1$c%1$cdone:%1$c  xor rax, rax%1$c  ret%1$c%1$csection .data%1$c  filename_format: db %2$cSully_%%d.s%2$c, 0%1$c  compile_cmd_format: db %2$cnasm -f elf64 -d CHILD Sully_%%1$d.s -o Sully_%%1$d.o && gcc -Wall -Wextra -Werror Sully_%%1$d.o -o Sully_%%1$d%2$c, 0%1$c  exec_cmd_format: db %2$c./Sully_%%d%2$c, 0%1$c  format: db %2$c%3$s%2$c, 0%1$c  filename: db 20 dup(0)%1$c  compile_cmd: db 100 dup(0)%1$c  exec_cmd: db 100 dup(0)%1$c%1$csection .note.GNU-stack%1$c", 0
+  format: db "extern dprintf%1$cextern snprintf%1$cextern system%1$c%1$csection .text%1$c  global main%1$c%1$cmain:%1$c  push rbp%1$c  mov rbp, rsp%1$c  sub rsp, 32%1$c%1$c  mov rcx, %4$d%1$c%1$c%%ifdef CHILD%1$c  dec rcx%1$c%%endif%1$c%1$c  cmp rcx, 0%1$c  jl done%1$c%1$c  push rcx ; save counter%1$c  push rcx ; save counter%1$c  push rcx ; save counter%1$c%1$c  ; create file name%1$c  lea rdi, [rel filename]%1$c  mov rsi, 20%1$c  lea rdx, [rel filename_format]%1$c  call snprintf wrt ..plt%1$c%1$c  ; open file%1$c  lea rdi, [rel filename]%1$c  mov rsi, 1101o ; O_CREAT | O_WRONLY | O_TRUNC%1$c  mov rax, 2     ; sys_open%1$c  syscall%1$c%1$c  cmp rax, 0%1$c  jl error%1$c  pop r9 ; restore counter for file format%1$c  push rax ; store file descriptor%1$c%1$c  ; write file%1$c  mov rdi, rax%1$c  lea rsi, [rel format]%1$c  mov rdx, 10%1$c  mov rcx, 34%1$c  lea r8, [rel format]%1$c  call dprintf wrt ..plt%1$c%1$c  ; close file%1$c  pop rdi    ; restore file descriptor%1$c  mov rax, 3 ; sys_close%1$c  syscall%1$c%1$c  ; run compile%1$c  lea rdi, [rel compile_cmd]%1$c  mov rsi, 100%1$c  lea rdx, [rel compile_cmd_format]%1$c  pop rcx ; restore counter%1$c  call snprintf wrt ..plt%1$c%1$c  lea rdi, [rel compile_cmd]%1$c  call system wrt ..plt%1$c%1$c  ; execute file%1$c  lea rdi, [rel exec_cmd]%1$c  mov rsi, 100%1$c  lea rdx, [rel exec_cmd_format]%1$c  pop rcx ; restore counter%1$c  call snprintf wrt ..plt%1$c%1$c  lea rdi, [rel exec_cmd]%1$c  call system wrt ..plt%1$c%1$c  jmp done%1$c%1$cerror:%1$c  mov rax, 1%1$c  leave%1$c  ret%1$c%1$cdone:%1$c  xor rax, rax%1$c  leave%1$c  ret%1$c%1$csection .data%1$c  filename_format: db %2$cSully_%%d.s%2$c, 0%1$c  compile_cmd_format: db %2$cnasm -f elf64 -d CHILD Sully_%%1$d.s -o Sully_%%1$d.o && gcc -Wall -Wextra -Werror Sully_%%1$d.o -o Sully_%%1$d%2$c, 0%1$c  exec_cmd_format: db %2$c./Sully_%%d%2$c, 0%1$c  format: db %2$c%3$s%2$c, 0%1$c  filename: db 20 dup(0)%1$c  compile_cmd: db 100 dup(0)%1$c  exec_cmd: db 100 dup(0)%1$c%1$csection .note.GNU-stack%1$c", 0
   filename: db 20 dup(0)
   compile_cmd: db 100 dup(0)
   exec_cmd: db 100 dup(0)
