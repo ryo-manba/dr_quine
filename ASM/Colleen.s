2b2417bdcd55b024a324f50c32a88122240e8ac9
Ryo Matsukawa <76232929+ryo-manba@users.noreply.github.com>
2024-10-12 11:13:20 +0900

    fix compile warning with .note.GNU-stack

--- ASM/Colleen.s
+++ ASM/Colleen.s
@@ -1,5 +1,4 @@
 section .text
-  default rel
   extern printf
   global main
 
@@ -12,13 +11,15 @@ main:
 
 ; sub function
 print:
-  mov	rdi, self
+  lea rdi, [rel self]
   mov	rsi, 10 ; newline
   mov rdx, 34 ; double quote
-  mov rcx, self
+  lea rcx, [rel self]
   mov	rax, 0
   call printf wrt ..plt
   ret
 
 section .data
-  self: db "section .text%1$c  default rel%1$c  extern printf%1$c  global main%1$c%1$cmain:%1$c  push rbp%1$c  call print%1$c  pop rbp%1$c  mov rax, 0%1$c  ret%1$c%1$c; sub function%1$cprint:%1$c  mov	rdi, self%1$c  mov	rsi, 10 ; newline%1$c  mov rdx, 34 ; double quote%1$c  mov rcx, self%1$c  mov	rax, 0%1$c  call printf wrt ..plt%1$c  ret%1$c%1$csection .data%1$c  self: db %2$c%3$s%2$c, 0%1$c", 0
+  self: db "section .text%1$c  extern printf%1$c  global main%1$c%1$cmain:%1$c  push rbp%1$c  call print%1$c  pop rbp%1$c  mov rax, 0%1$c  ret%1$c%1$c; sub function%1$cprint:%1$c  lea rdi, [rel self]%1$c  mov	rsi, 10 ; newline%1$c  mov rdx, 34 ; double quote%1$c  lea rcx, [rel self]%1$c  mov	rax, 0%1$c  call printf wrt ..plt%1$c  ret%1$c%1$csection .data%1$c  self: db %2$c%3$s%2$c, 0%1$c%1$csection .note.GNU-stack%1$c", 0
+
+section .note.GNU-stack
--- ASM/Grace.s
+++ ASM/Grace.s
@@ -1,5 +1,5 @@
 %define FILENAME "Grace_kid.s"
-%define SELF "%%define FILENAME %2$cGrace_kid.s%2$c%1$c%%define SELF %2$c%3$s%2$c%1$c%1$c%%macro MAIN 0%1$cextern dprintf%1$csection .text%1$c  global main%1$c%1$cmain:%1$c  lea rdi, [rel filename]%1$c  mov rsi, 1101o%1$c  mov rax, 2%1$c  syscall%1$c%1$c  cmp rax, 0%1$c  jl error%1$c%1$c  push rax%1$c%1$c  mov rdi, rax%1$c  lea rsi, [rel format]%1$c  mov rdx, 10%1$c  mov rcx, 34%1$c  lea r8, [rel format]%1$c  call dprintf wrt ..plt%1$c%1$c  pop rdi%1$c  mov rax, 3%1$c  syscall%1$c%1$c  xor rax, rax%1$c  ret%1$c%1$cerror:%1$c  mov rax, 1%1$c  ret%1$c%1$csection .data%1$c  format: db SELF, 0%1$c  filename: db FILENAME, 0%1$c%%endmacro%1$c%1$c; quine using macros%1$cMAIN%1$c"
+%define SELF "%%define FILENAME %2$cGrace_kid.s%2$c%1$c%%define SELF %2$c%3$s%2$c%1$c%1$c%%macro MAIN 0%1$cextern dprintf%1$csection .text%1$c  global main%1$c%1$cmain:%1$c  lea rdi, [rel filename]%1$c  mov rsi, 1101o%1$c  mov rax, 2%1$c  syscall%1$c%1$c  cmp rax, 0%1$c  jl error%1$c%1$c  push rax%1$c%1$c  mov rdi, rax%1$c  lea rsi, [rel format]%1$c  mov rdx, 10%1$c  mov rcx, 34%1$c  lea r8, [rel format]%1$c  call dprintf wrt ..plt%1$c%1$c  pop rdi%1$c  mov rax, 3%1$c  syscall%1$c%1$c  xor rax, rax%1$c  ret%1$c%1$cerror:%1$c  mov rax, 1%1$c  ret%1$c%1$csection .data%1$c  format: db SELF, 0%1$c  filename: db FILENAME, 0%1$c%1$csection .note.GNU-stack%1$c%%endmacro%1$c%1$c; quine using macros%1$cMAIN%1$c"
 
 %macro MAIN 0
 extern dprintf
@@ -38,6 +38,8 @@ error:
 section .data
   format: db SELF, 0
   filename: db FILENAME, 0
+
+section .note.GNU-stack
 %endmacro
 
 ; quine using macros
